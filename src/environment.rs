3068ac6ec872d3d65ee59e84f3ca85e0997c594f
Victor <v.cornillere@gmail.com>
2019-08-03 01:13:26 +0200

    Add resource for storing prefabs

--- src/environment.rs
+++ src/environment.rs
@@ -1,12 +1,31 @@
-use crate::player::Direction;
+use crate::{player::Direction, prefabs::SpritePrefabData};
+use amethyst::assets::{Handle, Prefab};
+use std::collections::HashMap;
 
 use serde::{Deserialize, Serialize};
 
+#[derive(Copy, Clone, PartialEq, Eq, Hash)]
 pub enum Tile {
     Nothing,
     Wall,
     Breakable,
     Pusher,
+    Ball,
+}
+
+#[derive(Default)]
+pub struct TilePrefabs {
+    prefabs: HashMap<Tile, Handle<Prefab<SpritePrefabData>>>,
+}
+
+impl TilePrefabs {
+    pub fn insert_prefab(&mut self, tile: Tile, prefab: Handle<Prefab<SpritePrefabData>>) {
+        self.prefabs.insert(tile, prefab);
+    }
+
+    pub fn get_prefab(&self, tile: Tile) -> Option<&Handle<Prefab<SpritePrefabData>>> {
+        self.prefabs.get(&tile)
+    }
 }
 
 #[derive(Default, Debug, Clone, Serialize, Deserialize)]
